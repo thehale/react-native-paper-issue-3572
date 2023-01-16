d3145d006114b34754e1e35a033b51298ef091e8
Joseph Hale <me@jhale.dev>
2023-01-16 15:35:43 -0700

    Add react-native-paper on PR#3596 to verify a successful fix.

--- App.tsx
+++ App.tsx
@@ -1,120 +1,36 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
+import { Appbar, Button, Provider } from 'react-native-paper';
+import React, { useState } from 'react';
 
-import React, {type PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import { View } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+export default function App() {
+  const [titleText, setTitleText] = useState('Press Me!');
+  const [pressCount, setPressCount] = useState(0);
 
-const Section: React.FC<
-  PropsWithChildren<{
-    title: string;
-  }>
-> = ({children, title}) => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+  function updateButton() {
+    let newCount = pressCount + 1;
+    setPressCount(newCount);
+    setTitleText(`Presses: ${newCount}`);
+  }
 
-const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+  // Auto-forcing a re-render doesn't have any effect.
+  if (pressCount == 0) updateButton();
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <Provider>
+      <Appbar.Header elevated={true} mode="center-aligned">
+        <Appbar.Content
+          title={
+            <View style={{ backgroundColor: 'red' }}>
+              <Button mode="contained-tonal" onPress={updateButton}>
+                {titleText}
+              </Button>
+            </View>
+          }
+          // titleStyle={{height: 40, top: 12}}  // A functional workaround to fix the issue 
+          // NOTE: This workaround is no longer needed once PR #3596 is merged.
+        />
+      </Appbar.Header>
+    </Provider>
   );
-};
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
-
-export default App;
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# React Native Paper | Issue #3572
+
+This repository serves as a minimal-reproducible example to validate the
+successful fix of [issue
+#3572](https://github.com/callstack/react-native-paper/issues/3572)
+via [PR #3596](https://github.com/callstack/react-native-paper/pull/3596)
+
+## License
+Unlicense (e.g. public domain to the extent allowed by law)
